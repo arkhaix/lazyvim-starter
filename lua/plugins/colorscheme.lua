a940e812eb1e058d2cc1f88b970f528ad7bcc34a
Andrew Gray <github@xeisnet.com>
2025-01-05 01:01:37 -0500

    Add colorschemes and switch to vscode scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+  { "martinsione/darkplus.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  { "navarasu/onedark.nvim" },
+  { "projekt0n/github-nvim-theme", name = "github" },
+  { "Mofiqul/vscode.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  {
+    "LazyVIM/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
