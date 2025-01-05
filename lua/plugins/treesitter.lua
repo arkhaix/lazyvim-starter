1e75c1f1c90ba0a16217ae7867942203911ebb94
Andrew Gray <github@xeisnet.com>
2025-01-05 01:12:49 -0500

    add treesitter parsers and delete example.lua

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "gitignore",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "powershell",
+        "python",
+        "rust",
+        "toml",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
